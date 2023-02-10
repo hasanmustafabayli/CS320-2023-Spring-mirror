877df81d7db050aaa99e6a639fb764159bdec1d0
hasanmustafabayli <88054843+hasanmustafabayli@users.noreply.github.com>
2023-02-10 16:27:30 -0500

    Update assign02_02.py

--- assigns/02/MySolution/assign02_02.py
+++ assigns/02/MySolution/assign02_02.py
@@ -2,6 +2,7 @@
 import sys
 sys.path.append('..')
 from assign02 import *
+from assign02_01 import *
 ####################################################
 print("[import ./../assign02.py] is done!")
 ####################################################
@@ -10,3 +11,23 @@ print("[import ./../assign02.py] is done!")
 # mylist_quicksort (see list_quicksort in assign02.sml)
 #
 ####################################################
+
+def mylist_quicksort(xs):
+    def qsort(xs):
+        if mylist_nilq(xs):
+            return mylist_nil()
+        x1 = xs.get_cons1()
+        ys, zs = qpart(xs.get_cons2(), x1)
+        return mylist_append(qsort(ys), mylist_cons(x1, qsort(zs)))
+    
+    def qpart(xs, p0):
+        if mylist_nilq(xs):
+            return (mylist_nil(), mylist_nil())
+        x1 = xs.get_cons1()
+        ys, zs = qpart(xs.get_cons2(), p0)
+        if x1 <= p0:
+            return (mylist_cons(x1, ys), zs)
+        else:
+            return (ys, mylist_cons(x1, zs))
+    
+    return qsort(xs)
