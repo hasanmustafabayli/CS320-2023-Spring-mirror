9b080f2efb941b92a2942d008e5fbe912345fb63
Hasan Mustafabayli <hasanbay@bu.edu>
2023-03-28 16:53:18 -0400

    hasan1

--- quizzes/part-03-28/part_03_28.py
+++ quizzes/part-03-28/part_03_28.py
@@ -101,7 +101,7 @@ def image_rotate_180r(image):
     hh = image.height
     return \
         imgvec.image_make_pylist\
-        (ww, hh, int1_map_pylist(hh*ww, lambda ij: imgvec.image_get_pixel(image,hh-1-(ij//ww), ww-1-(ij%ww))))
+        (hh, ww, int1_map_pylist(hh*ww, lambda ij: imgvec.image_get_pixel(image,hh-1-(ij//ww), ww-1-(ij%ww))))
 ####################################################
 # balloons = \
 #     load_color_image\
