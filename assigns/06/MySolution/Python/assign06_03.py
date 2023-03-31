3d53acd6320ab6dd4fcc46589bb08ae91cafc25e
Hasan Mustafabayli <hasanbay@bu.edu>
2023-03-30 22:34:46 -0400

    done

--- assigns/06/MySolution/Python/assign06_03.py
+++ assigns/06/MySolution/Python/assign06_03.py
@@ -4,17 +4,13 @@
 import sys
 sys.path.append('./../../../../mypylib')
 from mypylib_cls import *
+import queue
 ####################################################
 """
 HX-2023-03-24: 20 points
 Solving the N-queen puzzle
 """
 ####################################################
-
-def gtree_dfs(nxs, work, N):
-    lambda: strcon_nil() if nxs.ctag == 0 else\
-    strcon_cons(nxs.cons1, gtree_dfs(fnlist_append(fgetboards(nxs.cons1, N)), nxs, work, N))
-                   
 def nqueen(bd):
     res = 0
     for j0 in bd:
@@ -29,24 +25,45 @@ def board_safety_all(bd):
 
 def board_safety_one(bd,i0):
     def helper(j0):
-        pi = bd(i0)
-        pj = bd(j0)
+        pi = bd[i0]
+        pj = bd[j0]
         return  pi != pj and abs(i0-j0) != abs(pi-pj)
     return int1_forall(i0,helper)
-
 def fgetboards(bd,N):
     if len(bd) > 0:
         boards = []
-        for i in range(1, N-1):
+        for i in range(1, N+1):
             new_board = list(bd)
             new_board.append(i)
-            if board_safety_all(tuple(new_board),N):
+            if board_safety_one(tuple(new_board),len(new_board)-1):
                 boards.append(tuple(new_board))
-            else:
-                boards = [(j,) for j in range(1, N+1)]
-            return pylist_fnlistize(boards)
+    else:
+        boards = [(j,) for j in range(1, N+1)]
+    return pylist_fnlistize(boards)
+
+def gtree_dfs(nxs, fchlds, N):
+    def helper(nxs):
+        if nxs.empty():
+            return strcon_nil()
+        else:
+            nx1 = nxs.get()
+            # print("gtree_bfs: helper: nx1 = ", nx1)
+            for nx2 in fchlds(nx1,N):
+                nxs.put(nx2)
+            return strcon_cons(nx1, gtree_dfs(nxs, fchlds, N)) 
+    return lambda: helper(nxs)
+
 
 def solve_N_queen_puzzle(N):
-    x = fgetboards((), N)
-    y = gtree_dfs(x, fgetboards,N)
-    return y
+    x = queue.LifoQueue()
+    b = fgetboards((), N)
+    for i in range(N):
+        y = b
+        b = b.cons1
+        x.put(b)
+        b = y.cons2
+    test = stream_make_filter(gtree_dfs(x, fgetboards, N), lambda w: len(w) ==N)
+    return test
+
+
+
