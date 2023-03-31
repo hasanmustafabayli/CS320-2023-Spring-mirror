fa0182190381a4adc002aa43416ef7331df5b3da
Hasan Mustafabayli <hasanbay@bu.edu>
2023-03-31 15:05:08 -0400

    final comment

--- assigns/06/MySolution/Python/assign06_03.py
+++ assigns/06/MySolution/Python/assign06_03.py
@@ -12,6 +12,15 @@ Solving the N-queen puzzle
 """
 ####################################################
 def nqueen(bd):
+    """
+    Takes a board configuration `bd` represented as a tuple of integers and returns the number of queens on the board.
+
+    Args:
+    - bd: A tuple of integers representing the board configuration.
+
+    Returns:
+    - The number of queens on the board.
+    """
     res = 0
     for j0 in bd:
         if j0 <= 0:
@@ -21,15 +30,44 @@ def nqueen(bd):
     return res
 
 def board_safety_all(bd):
+    """
+    Takes a board configuration `bd` represented as a tuple of integers and checks whether all the queens are safe.
+
+    Args:
+    - bd: A tuple of integers representing the board configuration.
+
+    Returns:
+    - 1 if all the queens are safe, 0 otherwise.
+    """
     return int1_forall(nqueen(bd), lambda i0: board_safety_one(bd,i0))
 
 def board_safety_one(bd,i0):
+    """
+    Takes a board configuration `bd` represented as a tuple of integers and an index `i0` of a queen on the board, and checks whether the queen at position `i0` is safe.
+
+    Args:
+    - bd: A tuple of integers representing the board configuration.
+    - i0: The index of the queen on the board.
+
+    Returns:
+    - 1 if the queen is safe, 0 otherwise.
+    """
     def helper(j0):
         pi = bd[i0]
         pj = bd[j0]
         return  pi != pj and abs(i0-j0) != abs(pi-pj)
     return int1_forall(i0,helper)
 def fgetboards(bd,N):
+    """
+    Takes a board configuration `bd` represented as a tuple of integers and a board size `N`, and generates all the possible board configurations that can be obtained by placing one more queen on the board.
+
+    Args:
+    - bd: A tuple of integers representing the board configuration.
+    - N: The size of the board.
+
+    Returns:
+    - A list of tuples representing all the possible board configurations that can be obtained by placing one more queen on the board.
+    """
     if len(bd) > 0:
         boards = []
         for i in range(1, N+1):
@@ -42,12 +80,22 @@ def fgetboards(bd,N):
     return pylist_fnlistize(boards)
 
 def gtree_dfs(nxs, fchlds, N):
+    """
+    Returns a generator function for performing depth-first search on a tree.
+    
+    Parameters:
+    nxs (Queue): A queue of nodes in the tree
+    fchlds (function): A function that generates child nodes of a given node
+    N (int): The size of the board
+    
+    Returns:
+    function: A generator function for performing depth-first search on the tree
+    """
     def helper(nxs):
         if nxs.empty():
             return strcon_nil()
         else:
             nx1 = nxs.get()
-            # print("gtree_bfs: helper: nx1 = ", nx1)
             for nx2 in fchlds(nx1,N):
                 nxs.put(nx2)
             return strcon_cons(nx1, gtree_dfs(nxs, fchlds, N)) 
@@ -55,14 +103,23 @@ def gtree_dfs(nxs, fchlds, N):
 
 
 def solve_N_queen_puzzle(N):
-    x = queue.LifoQueue()
-    b = fgetboards((), N)
+    """
+    Solves the N-Queens puzzle using depth-first search.
+    
+    Parameters:
+    N (int): The size of the board
+    
+    Returns:
+    stream: A stream of solutions to the N-Queens puzzle
+    """
+    man = queue.LifoQueue()
+    new = fgetboards((), N)
     for i in range(N):
-        y = b
-        b = b.cons1
-        x.put(b)
-        b = y.cons2
-    test = stream_make_filter(gtree_dfs(x, fgetboards, N), lambda w: len(w) ==N)
+        ls = new
+        new = new.cons1
+        man.put(new)
+        new = ls.cons2
+    test = stream_make_filter(gtree_dfs(man, fgetboards, N), lambda w: len(w) ==N)
     return test
 
 
--- assigns/06/MySolution/Python/assign06_04.py
+++ assigns/06/MySolution/Python/assign06_04.py
@@ -34,29 +34,47 @@ tuples are always enumerated ahead of longer ones.
 ######
 """
 ####################################################
-def word_neighbors(word):
-    def AB():
+def get_word_neighbors(word):
+    """This function takes a string word as input and returns a functional list (fnlist) of all the strings 
+    that can be created by changing a single character in word to another character in the alphabet. 
+    For example, if word is "cat", the function will return an fnlist of the following strings: 
+    ["aat", "bat", "cat", "dat", ..., "caz"]. The function uses two nested string_imap_fnlist() 
+    calls to generate all the possible neighbors."""
+    def get_alphabets():
         return "abcdefghijklmnopqrstuvwxyz"
+    
     def replace_char_at(s, index, c):
         return s[:index] + c + s[index+1:]
 
-    wlen = len(word)
+    word_len = len(word)
+    
     return fnlist_concat(
         string_imap_fnlist(word, lambda i, c:
             fnlist_concat(
-                string_imap_fnlist(AB(), lambda _, c1:
+                string_imap_fnlist(get_alphabets(), lambda _, c1:
                     fnlist_sing(replace_char_at(word, i, c1))
                     if c != c1 else fnlist_nil()))))
 
-def word_neighbors_legal(word):
-    return fnlist_filter_pylist(word_neighbors(word), word_is_legal)
+def get_word_neighbors_legal(word):
+    """This function takes a string word as input and returns an fnlist of all the legal words that can 
+    be created by changing a single character in word to another character in the alphabet. 
+    The function uses the get_word_neighbors() function to generate all the possible neighbors of word, 
+    and then filters out any neighbors that are not legal words using the word_is_legal() function."""
+    return fnlist_filter_pylist(get_word_neighbors(word), word_is_legal)
 
-def wpath_neighbors_legal(wpath):
+def get_wpath_neighbors_legal(wpath):
+    """This function takes a tuple wpath as input, which represents a sequence of words, and returns a list of all the possible extensions of wpath that 
+    end in a legal word. The function gets the last word in wpath, generates all the possible legal neighbors 
+    of that word using the get_word_neighbors_legal() function, and then appends each neighbor to wpath to create a new path."""
     word1 = wpath[-1]
-    words = word_neighbors_legal(word1)
+    words = get_word_neighbors_legal(word1)
     return [wpath + (word2,) for word2 in words]
 
-def gtree_bfs(nxs, fchildren):
+def get_gtree_bfs(nxs, fchildren):
+    """This function takes a queue nxs and a function fchildren as inputs, and returns a lambda function that 
+    performs a breadth-first search (BFS) of a tree. The function repeatedly pops an element 
+    from the queue, applies fchildren() to generate its children, and adds them to the end of the queue. 
+    The function returns an fnlist of all the elements in the tree in BFS order."""
     def helper(nxs):
         if nxs.empty():
             return strcon_nil()
@@ -64,12 +82,20 @@ def gtree_bfs(nxs, fchildren):
             nx1 = nxs.get()
             for nx2 in fchildren(nx1):
                 nxs.put(nx2)
-            return strcon_cons(nx1, gtree_bfs(nxs,fchildren))
+            return strcon_cons(nx1, get_gtree_bfs(nxs,fchildren))
     return lambda: helper(nxs)
 
+
 def doublet_stream_from(word):
+    """"This function takes a string word as input and returns a lambda function that 
+    generates a stream of all the possible doublets (pairs of words) that can be formed 
+    by changing a single letter at a time from word to another legal word. 
+    The function uses the gtree_bfs() function to perform a BFS of a tree whose nodes are sequences of words, 
+    starting with the initial sequence [word], and whose edges are formed by adding a legal neighbor to the end of a sequence.
+      The lambda function returned by doublet_stream_from() generates the doublets by iterating over the BFS fnlist and 
+      returning pairs of consecutive elements."""  
     nxs = queue.Queue(); nxs.put((word,))
-    return gtree_bfs(nxs, wpath_neighbors_legal)
+    return get_gtree_bfs(nxs, get_wpath_neighbors_legal)
 
 stream_iforall(doublet_stream_from("water"), lambda i,x: i <100 and not(print(x)))
     
